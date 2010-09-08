e19ec260c7e88ffe9a01612737a9db5502af934b
Jens Geiregat <Jens.Geiregat@Oracle.com>
2010-09-08 13:18:02 +0200

    Add support for setuptools

---
+++ pier/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+import pier
+
+setup(
+        name="pier",
+        version=pier.__version__,
+        description="A Python docstring generator",
+        author="Jens Geiregat",
+        author_email="jens.geiregat@gmail.com",
+        url="http://github.com/RealNitro/pier",
+        license="GPL (v3 or later)",
+        py_modules=['pier']
+        )
